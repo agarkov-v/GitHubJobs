75d0a742c64b4bf13b3880ba6b0bd27c78c0e0ca
Agarkov Vyacheslav <avnlife8@gmail.com>
2020-12-02 14:14:06 +0300

    init

---
+++ GitHubJobs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubJobs
+//
+//  Created by Вячеслав Агарков on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
